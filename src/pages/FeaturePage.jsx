8b1be92bcdaf067e7323247eca168f0cec9f88e1
Priyanshu Vats <priyanshuvatsbal@gmail.com>
2025-04-16 18:32:00 +0530

    new page IntegeratePage and new comp Slider usinig marquee issue in passing of width in bttn comp

--- src/pages/FeaturePage.jsx
+++ src/pages/FeaturePage.jsx
@@ -22,21 +22,21 @@ const FeaturePage = () => {
 
   return (
     <div className='h-[130vh] w-full flex justify-center items-center overflow-hidden'>
-        {/* <div className="center bg-black h-[90%] w-[92%] rounded-[4vw] flex justify-center items-center">
-          <div className="w-[96%]  h-[94%] flex">
-           <div className="left h-[100%] w-[50%] rounded-[2vw] ">
-            <div className="l-top w-[100%] h-[50%]">
+    {/* //     <div className="center bg-black h-[90%] w-[92%] rounded-[4vw] flex justify-center items-center">
+    //       <div className="w-[96%]  h-[94%] flex">
+    //        <div className="left h-[100%] w-[50%] rounded-[2vw] ">
+    //         <div className="l-top w-[100%] h-[50%]">
                  
-            </div>
-            <div className="l-bot">
+    //         </div>
+    //         <div className="l-bot">
 
-            </div>
-           </div>
-           <div className="right h-[100%] w-[50%] rounded-[2vw]  border-2 border-[#696969] bg-[#191919]"></div>
-           </div>
-        </div> */}
+    //         </div>
+    //        </div>
+    //        <div className="right h-[100%] w-[50%] rounded-[2vw]  border-2 border-[#696969] bg-[#191919]"></div>
+    //        </div>
+    //     </div> */}
         
-            <img src="https://lunchbox.io/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ferglja63hoxc%2F3zccmzKjGdVRyUmwCTDGWj%2F086d7bd5334fc86255eab6d9cea7f9da%2F8_OpenAPI_Features_Website.png&w=1920&q=75"></img>
+            <img src="https://lunchbox.io/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ferglja63hoxc%2F3zccmzKjGdVRyUmwCTDGWj%2F086d7bd5334fc86255eab6d9cea7f9da%2F8_OpenAPI_Features_Website.png&w=1920&q=75"></img> 
         
 
     </div>
